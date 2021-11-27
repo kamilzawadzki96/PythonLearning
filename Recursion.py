51cdea9475ef8b5c25af71aa16c08b57485547cf
Kamil Zawadzki <kamil@macbook-air-kamil.home>
2021-11-27 21:08:09 +0100

    Recursion, fibbonaci

--- Recursion.py
+++ Recursion.py
@@ -4,11 +4,11 @@ def function(x):
     else:
         return x * function(x-1)
 
-print(function(5))
+print("Factorial:", function(5))
 
 def fib(x):
   if x == 0 or x == 1:
     return 1
   else: 
     return fib(x-1) + fib(x-2)
-print(fib(4))
+print("Fibbonaci:", fib(5))
