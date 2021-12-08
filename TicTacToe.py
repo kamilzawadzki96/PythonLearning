bed4076b0958f4bcb79f03e552fc54f43385f395
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2021-12-08 12:27:33 +0100

    Tic Tac Toe v2

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,7 +1,13 @@
 import os
 
 board = [[1, 2, 3], [4, 5, 6], [7 ,8 ,9]]
+
 xory = 0
+
+#reset board
+def resetBoard():
+    list = [[1, 2, 3], [4, 5, 6], [7 ,8 ,9]]
+    return list
 
 #print tab
 def printBoard():
@@ -14,8 +20,26 @@ def printBoard():
         if i == 2:
             print(3* " ---")
 
+def printWinner(win):
+    print("Wygrywa:", win)
+
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
+
+    #definition of winner
+    for i in range (0,3):
+        if board[i][0] == board[i][1] == board[i][2] or board[0][i] == board[1][i] == board[2][i]:
+            printWinner(board[i][0])
+            board = resetBoard()
+            xory = 0
+
+    #diagonally check
+    if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
+        printWinner(board[0][0])
+        board = resetBoard()
+        xory = 0    
+
+    #print board
     printBoard()
 
     #input index
