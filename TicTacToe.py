c122762e3bbb7225ff0475ebff70d88a31d9a1ce
kamilzawadzki96 <kamil.zawadzki96@outlook.com>
2021-12-08 20:44:41 +0100

    Tic Tac Toe added player turn info

--- TicTacToe.py
+++ TicTacToe.py
@@ -27,7 +27,7 @@ def printBoard():
             print(3* " ---")
 
 def printWinner(win):
-    print("Wygrywa:", win)
+    print("And the winner is:", win)
 
 while True:
     #definition of winner
@@ -50,6 +50,12 @@ while True:
     #print board
     os.system('cls' if os.name == 'nt' else 'clear')
     printBoard()
+
+    #display which player turn it is
+    if xory == 0:
+        print("\nIt's X turn\n")
+    else:
+        print("\nIt's Y turn\n")
 
     #input index
     try:
