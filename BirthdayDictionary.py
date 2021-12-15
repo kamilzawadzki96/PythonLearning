16c357094089db48341d11f38a15c8385158e0f4
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2021-12-15 12:15:43 +0100

    Dictionary Exercise

---
+++ BirthdayDictionary.py
@@ -0,0 +1,16 @@
+#####
+
+#Exercise link: https://www.practicepython.org/exercise/2017/01/24/33-birthday-dictionaries.html
+
+#####
+
+birthdays = {
+    'firstname' : 'Kamil',
+    'lastname' : 'Kamil',
+    'birthday' : '1996-09-23'
+}
+
+print(birthdays.get('birthday'))
+
+birthdays['firstname'] = 'Stefan'
+print(birthdays.get('firstname'))
