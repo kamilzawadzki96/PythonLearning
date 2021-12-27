204b71353644dba2c7236bef05f631da8e70cc4b
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2021-12-27 11:43:59 +0100

    Decode wykop hity

--- DecodeWeb.py
+++ DecodeWeb.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-base_url = 'https://www.wykop.pl/'
+base_url = 'https://www.wykop.pl/hity/dnia/'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text)
- 
-for story_heading in soup.find_all("h2"): 
-    if story_heading.a: 
-        print(story_heading.a.text.replace("\n", " ").strip())
-        print(story_heading.a.get("href"))
-    else: 
-        print(story_heading.contents[0].strip())
+content = soup.select('li[class*="link iC"]')
+
+wykoplist = []
+
+for article in content:
+
+    article_rating = article.span.contents[0]
+    article_header = article.h2.a.contents[0]
+    article_link = article.h2.a.get("href")
+    row = [article_rating, article_header, article_link]
+    wykoplist.append(row)
+
+print(wykoplist)
