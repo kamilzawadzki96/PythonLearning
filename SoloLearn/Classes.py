46f7488ef0cd0cbad5cbc5271f8d766d3127dbf9
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-01-05 16:51:39 +0100

    Classes

--- SoloLearn/Classes.py
+++ SoloLearn/Classes.py
@@ -3,8 +3,13 @@ class Cat:
     self.color = color
     self.legs = legs
 
+  def penis(self):
+      print("PENIS!!!")
+
 felix = Cat("ginger", 4)
 rover = Cat("dog-colored", 4)
 stumpy = Cat("brown", 3)
 
 print(stumpy.color)
+
+stumpy.penis()
