024f96a0ea9454ddb3a454d411aab570d7912bef
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-01-10 10:14:25 +0100

    Tkinter

---
+++ Tkinter.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import tkinter.scrolledtext as tkst
+
+# Create instance
+parent = tk.Tk()
+# Add a title
+parent.title("Wykop.pl Hity!")
+
+parent.geometry('350x200')
+txt = tkst.ScrolledText(parent, width=40, height=10)
+txt.grid(column=0, row=0)
+
+# Start GUI
+parent.resizable(1,1)
+parent.mainloop()
