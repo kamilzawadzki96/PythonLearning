5bcffe82a010e8791bcc80d12a2a014f7e7aec0d
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-01-10 12:10:17 +0100

    Tkinter

--- Tkinter.py
+++ Tkinter.py
@@ -9,6 +9,8 @@ parent.title("Wykop.pl Hity!")
 parent.geometry('350x200')
 txt = tkst.ScrolledText(parent, width=40, height=10)
 txt.grid(column=0, row=0)
+txt.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+txt.insert("insert","Hello world!")
 
 # Start GUI
 parent.resizable(1,1)
