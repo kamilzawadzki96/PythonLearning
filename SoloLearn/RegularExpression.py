c4a70436c0911badddb0baa7f90f3339fc71ed15
Kamil Zawadzki <kamil@macbook-air-kamil.home>
2022-01-14 22:09:56 +0100

    Regular Expressions

--- SoloLearn/RegularExpression.py
+++ SoloLearn/RegularExpression.py
@@ -2,12 +2,10 @@ import re
 
 pattern = r"spam"
 
-"""
 if re.search(pattern, "asdaspamfadsdfas"):
     print('Match')
 else:
     print("Doesn't match!")
-"""
 
 #print(re.findall(pattern, "eggsspamspammilk"))
 
@@ -17,3 +15,9 @@ print(match.end())
 print(match.start())
 print(match.group())
 print(match.span())
+
+pattern = r"Ala"
+str = "Ala ma kota!"
+newstr = re.sub(pattern, "Julian", str)
+
+print(newstr)
