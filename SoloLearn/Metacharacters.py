de228f98003c38b170473979ac0086d1f6da4b0c
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-01-18 10:15:27 +0100

    Metachar

--- SoloLearn/Metacharacters.py
+++ SoloLearn/Metacharacters.py
@@ -16,3 +16,22 @@ if re.match(pattern, "blues"):
 
 if re.match(pattern, "red"):
     print("Match 4")
+
+print("-------------------------------------")
+
+pattern = r"[a-z]...[h]"
+
+if re.match(pattern, "grey"):
+    print("Match 1")
+
+if re.match(pattern, "qwertyuiop"):
+    print("Match 2")
+
+if re.match(pattern, "rasdh"):
+    print("Match 3")
+
+if re.match(pattern, "aeiouaasd"):
+    print("Match 4")
+
+if re.match(pattern, "Red"):
+    print("Match 5")
