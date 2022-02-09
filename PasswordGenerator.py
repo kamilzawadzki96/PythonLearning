4ea8a22424c8644df41566eb1a09d30110bb5c28
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-02-09 18:44:30 +0000

    Password Generator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,6 +1,8 @@
 import random
+import string
 
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+#s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+s = string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
 passlen = 14
 p =  "".join(random.sample(s,passlen ))
 print(p)
