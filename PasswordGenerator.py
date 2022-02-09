e9beb8e5592a90506c3d249d1f7345aac9b02fba
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-02-09 19:26:14 +0000

    Password Generator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -12,7 +12,7 @@ print(p)
 special_signs = '!@#$'
 
 p = "".join(random.sample(string.ascii_uppercase,1) + random.sample(string.ascii_lowercase,4) + random.sample(special_signs,1) + random.sample(string.digits, 4))
-print(str(p))
+print(p)
 
 #Passowrd with random word
 p = RandomWords().get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=1, maxCorpusCount=10, minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10).capitalize() + "".join(random.sample(special_signs,1) + random.sample(string.digits, 4))
