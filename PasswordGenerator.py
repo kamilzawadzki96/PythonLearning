0df0d2e7c412f08907e03208a1ebea61582b2946
Kamil Zawadzki <kamil.zawadzki96@outlook.com>
2022-02-09 19:18:16 +0000

    Password Generator

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,8 +1,23 @@
 import random
 import string
+from random_word import RandomWords
 
-#s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+#Tottaly random password
 s = string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
 passlen = 14
 p =  "".join(random.sample(s,passlen ))
+print(p)
+
+#Password with multiple characters
+special_signs = '!@#$'
+
+p = "".join(random.sample(string.ascii_uppercase,1) + random.sample(string.ascii_lowercase,4) + random.sample(special_signs,1) + random.sample(string.digits, 4))
+print(str(p))
+
+#Passowrd with random word
+p = RandomWords()
+p = p.get_random_word(hasDictionaryDef="true", includePartOfSpeech="noun,verb", minCorpusCount=1, maxCorpusCount=10, minDictionaryCount=1, maxDictionaryCount=10, minLength=5, maxLength=10)
+p = p.capitalize()
+p = p + "".join(random.sample(special_signs,1) + random.sample(string.digits, 4))
+
 print(p)
